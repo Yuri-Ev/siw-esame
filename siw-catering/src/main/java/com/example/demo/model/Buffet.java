e30b571952b485d343981edf379e2eb1cab6fe41
Yuri <yur.evangelista@stud.uniroma3.it>
2022-06-17 03:12:48 +0200

    Update Buffet.java

---
+++ siw-catering/src/main/java/com/example/demo/model/Buffet.java
@@ -0,0 +1,51 @@
+package com.example.demo.model;
+
+import java.util.List;
+
+import javax.persistence.*;
+
+@Entity
+public class Buffet {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+	
+	@Column(nullable = false)
+	private String nome;
+	
+	@Column(length = 100)
+	private String descrizione;
+
+	@ManyToOne
+	private Chef propositore;
+	
+	@OneToMany
+	private List<Piatto> piattiProposti;
+	
+	
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescrizione() {
+		return descrizione;
+	}
+
+	public void setDescrizione(String descrizione) {
+		this.descrizione = descrizione;
+	}
+
+}
