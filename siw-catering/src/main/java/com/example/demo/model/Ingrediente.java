ef742a4485ad01f0459142cdb634a920033d0f62
Yuri <yur.evangelista@stud.uniroma3.it>
2022-06-17 03:13:10 +0200

    Create Ingrediente.java

---
+++ siw-catering/src/main/java/com/example/demo/model/Ingrediente.java
@@ -0,0 +1,42 @@
+package com.example.demo.model;
+
+import javax.persistence.*;
+
+@Entity
+public class Ingrediente {
+	
+	@Id
+	private String nome;
+	
+	private String origine;
+	
+	@Column(length = 100)
+	private String descrizione;
+
+
+	
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getOrigine() {
+		return origine;
+	}
+
+	public void setOrigine(String origine) {
+		this.origine = origine;
+	}
+
+	public String getDescrizione() {
+		return descrizione;
+	}
+
+	public void setDescrizione(String descrizione) {
+		this.descrizione = descrizione;
+	}
+
+}
